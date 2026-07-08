d70ddbbd709f1c998df3019946bdc418370c07ec
AsceGamer <yginho.ya@gmail.com>
2026-07-08 16:02:49 -0300

    08/07/2026 pt5

--- EXERCICIOS-JS/Exercicios__Javascriptpdf/Exercicios-CursoFundamentosdeProgramacao.js
+++ EXERCICIOS-JS/Exercicios__Javascriptpdf/Exercicios-CursoFundamentosdeProgramacao.js
@@ -400,11 +400,27 @@ console.log(menorNumero([15, 25, 80, 100]));
 
 /*22) Desenvolva uma função que receba como parâmetro um número de 1 a 10. Internamente, na função, será gerado um número aleatório de 1 a 10. A função deverá retornar se o parâmetro de entrada foi igual ao número sorteado internamente. Se o valor fornecido foi o sorteado, retorne "Parabéns! O número sorteado foi o Х". Se não for igual, retorne "Que pena! O número sorteado foi o Х". Х é o número que foi sorteado.
 Exemplos:
-funcaoDaSorte(10) // retornará "Parabéns! O número sorteado foi o 10" funcaoDaSorte(5) 
-// retornará "Que pena! O número sorteado foi o 3" funcaoDaSorte(5) // retornará "Que pena! O número sorteado foi o 1"
+funcaoDaSorte(10) // retornará "Parabéns! O número sorteado foi o 10" 
+funcaoDaSorte(5) // retornará "Que pena! O número sorteado foi o 3" 
+funcaoDaSorte(5) // retornará "Que pena! O número sorteado foi o 1"
 */
 
+function funcaoDaSorte(numero) {
+    const numeroSorteado = Math.floor(Math.random() * 10) + 1;
+    if (numero >= 1 && numero <= 10) {
+        if (numero === numeroSorteado) {
+            return `Parabéns! O número sorteado foi o ${numeroSorteado}`;
+        } else {
+            return `Que pena! O número sorteado foi o ${numeroSorteado} e o número informado foi o ${numero}`;
+        };
+    } else {
+        return "Número inválido. Por favor, insira um número entre 1 e 10.";
+    };
+};
 
+console.log(funcaoDaSorte(10));
+console.log(funcaoDaSorte(5));
+console.log(funcaoDaSorte(12));
 
 /*23) Criar uma função que receba uma string como parâmetro e conte quantas palavras tem nela.
 Considere que todas as palavras só são separadas por um espaço.
