46f2f57e6b68cabed8e918053c6418e09ef642e4
AsceGamer <yginho.ya@gmail.com>
2026-07-08 15:28:53 -0300

    08/07/2026 pt2

--- EXERCICIOS-JS/Exercicios__Javascriptpdf/Exercicios-CursoFundamentosdeProgramacao.js
+++ EXERCICIOS-JS/Exercicios__Javascriptpdf/Exercicios-CursoFundamentosdeProgramacao.js
@@ -358,7 +358,14 @@ calcularMedia([0, 10]) // retornará 5
 calcularMedia([1, 2, 3, 4, 5]) // retornará 3
 */
 
+function calcularMedia(array) {
+    const soma = array.reduce((acc, num) => acc + num, 0);
+    return soma / array.length;
+}
 
+console.log(calcularMedia([0, 10]));
+console.log(calcularMedia([1, 2, 3, 4, 5]));
+console.log(calcularMedia([15, 25, 80, 100]));
 
 /*20) Faça uma função que recebe a base e a altura de um triângulo e retorne a área desse triângulo. A precisão deverá ser de duas casas decimais, arredondando se necessário.
 Obs: a fórmula para calcular a área de um triângulo é (base x altura} / 2
