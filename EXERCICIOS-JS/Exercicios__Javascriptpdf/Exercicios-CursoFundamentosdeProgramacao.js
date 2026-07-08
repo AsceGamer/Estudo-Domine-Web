15ad5456d560bf0102f306b85b1d25e4c7e67311
AsceGamer <yginho.ya@gmail.com>
2026-07-08 15:35:59 -0300

    08/07/2026 pt3

--- EXERCICIOS-JS/Exercicios__Javascriptpdf/Exercicios-CursoFundamentosdeProgramacao.js
+++ EXERCICIOS-JS/Exercicios__Javascriptpdf/Exercicios-CursoFundamentosdeProgramacao.js
@@ -375,7 +375,13 @@ areaDoTriangulo(7, 9) // retornará 31.50
 areaDoTriangulo(9.25, 13.1) // retornará 60.59
 */
 
+function areaDoTriangulo(base, altura) {
+    return +((base * altura) / 2).toFixed(2);    // o operador + é usado para converter a string resultante de toFixed() de volta para um número
+}
 
+console.log(areaDoTriangulo(10, 15));
+console.log(areaDoTriangulo(7, 9));
+console.log(areaDoTriangulo(9.25, 13.1));
 
 /*21) Criar uma função que receba um array de números e retorne o menor número desse array. 
 Exemplos:
