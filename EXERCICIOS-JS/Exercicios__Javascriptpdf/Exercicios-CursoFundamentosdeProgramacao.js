2c9668d5972798683134df67ff085348451a55f4
AsceGamer <yginho.ya@gmail.com>
2026-07-08 15:51:05 -0300

    08/07/2026 pt4

--- EXERCICIOS-JS/Exercicios__Javascriptpdf/Exercicios-CursoFundamentosdeProgramacao.js
+++ EXERCICIOS-JS/Exercicios__Javascriptpdf/Exercicios-CursoFundamentosdeProgramacao.js
@@ -389,7 +389,14 @@ menorNumero([10, 5, 35, 65]) // retornará 5
 menorNumero([5, -15, 50, 3]) // retornará -15
 */
 
+function menorNumero(array) {
+    return array.reduce((menor, atual) => atual < menor ? atual : menor);
+    //return Math.min(...array);
+}
 
+console.log(menorNumero([10, 5, 35, 65]));
+console.log(menorNumero([5, -15, 50, 3]));
+console.log(menorNumero([15, 25, 80, 100]));
 
 /*22) Desenvolva uma função que receba como parâmetro um número de 1 a 10. Internamente, na função, será gerado um número aleatório de 1 a 10. A função deverá retornar se o parâmetro de entrada foi igual ao número sorteado internamente. Se o valor fornecido foi o sorteado, retorne "Parabéns! O número sorteado foi o Х". Se não for igual, retorne "Que pena! O número sorteado foi o Х". Х é o número que foi sorteado.
 Exemplos:
