c1f5f23a90372421cfff9b5a0b5122d57672a4f4
AsceGamer <yginho.ya@gmail.com>
2026-07-30 17:35:36 -0300

    30/07/2026 pt2

--- EXERCICIOS-JS/node/funcionarios/funcionarios.js
+++ EXERCICIOS-JS/node/funcionarios/funcionarios.js
@@ -1,13 +1,21 @@
 const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
 const axios = require("axios");
 
+axios.get(url).then((response) => {
+    const funcionarios = response.data;
+});
+
 const chineses = (func) => func.pais === "China";
 const mulheres = (func) => func.genero === "F";
-const salarioMaior = (func) => func.salario > 2100;
 
 console.log("Mulheres chinesas:");
 axios.get(url).then((response) => {
   const funcionarios = response.data;
-    const resultado = funcionarios.filter(chineses).filter(mulheres).filter(salarioMaior);
+    const resultado = funcionarios.filter(chineses).filter(mulheres).reduce((acc, func) => {
+      if (func.salario < acc.salario) {
+        return func;
+      }
+      return acc;
+    }, funcionarios[0]);
     console.log(resultado);
 });
