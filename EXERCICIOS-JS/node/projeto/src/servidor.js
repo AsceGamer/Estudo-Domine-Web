0b9779aa33f371ccc0b47d9d1745a3541a9296e3
AsceGamer <yginho.ya@gmail.com>
2026-08-21 18:49:55 -0300

    21/08/2026

--- EXERCICIOS-JS/node/projeto/src/servidor.js
+++ EXERCICIOS-JS/node/projeto/src/servidor.js
@@ -2,7 +2,10 @@ const porta = 3003;
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const bancoDeDados = require('./bancoDeDados');
+
+app.use(bodyParser.urlencoded({ extended: true }));  // middleware para tratar o corpo da requisição
 
 app.get('/produtos', (req, res, next) => {          // apagando produtos e colocando user, vai passar a responder para qualquer outra requisição
     res.send(bancoDeDados.getProdutos());            // Converter para JSON
@@ -18,6 +21,20 @@ app.post('/produtos', (req, res, next) => {
         preco: req.body.preco
     });
     res.send(produto);    // JSON       
+});
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+    res.send(produto);
+});
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id);
+    res.send(produto);
 });
 
 app.listen(porta, () => {
